d7e6160d090a37b3844ecdcb6edbfd5708c1ca66
zulya <zulya.kemerovo@gmail.com>
2025-09-01 11:33:29 +0700

    edit test

---
+++ src/test/java/com/example/store/TestContainerInitialization.java
@@ -0,0 +1,29 @@
+package com.example.store;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestContainerInitialization {
+
+    public static final PostgreSQLContainer<?> postgresqlContainer;
+
+    static {
+        postgresqlContainer = new PostgreSQLContainer<>("postgres:15")
+                .withDatabaseName("integration-tests-db")
+                .withUsername("sa").withPassword("sa");
+        postgresqlContainer.start();
+    }
+
+    @DynamicPropertySource
+    public static void setupProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> "jdbc:tc:postgresql:15:///integration-tests-db");
+        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+
+}
